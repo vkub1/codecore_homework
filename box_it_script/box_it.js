6b4cb3b33a82aeca77078f9346550713f6402fed
vkub1 <kubliyvlad@gmail.com>
2021-09-03 16:28:37 -0700

    added drawTopBorder function

--- box_it_script/box_it.js
+++ box_it_script/box_it.js
@@ -2,3 +2,15 @@ function drawLine(num) {
     return "━".repeat(num);
 }
 
+function drawTopBorder(num) {
+    if (num === 0) {
+        return '┏┓';
+    } else {
+        return '┏' + drawLine(num-2) + '┓';
+    }
+}
+
+
+
+console.log(drawLine(0));
+console.log(drawTopBorder(2));
