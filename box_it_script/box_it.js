d87cd44d00e2dc632e5b02ef2b1c25da79195918
vkub1 <kubliyvlad@gmail.com>
2021-09-18 16:29:13 -0700

    initial commit hw3

--- box_it_script/box_it.js
+++ box_it_script/box_it.js
@@ -18,7 +18,7 @@ function drawMiddleBorder(num) {
     }
 }
 
-function drawBottomBorder() {
+function drawBottomBorder(num) {
     if (num === 0) {
         return '┗┛';
     } else {
@@ -26,7 +26,36 @@ function drawBottomBorder() {
     }
 }
 
+function drawBarsAround(str) {
+    return `┃${str}┃`;
+}
 
+function boxIt(arr) {
+    if (arr.length === 0) {
+        return drawTopBorder(0) + "\n" + drawBottomBorder(0);
+    }
+    else {
+        let max = arr[0].length;
+        let result = ""
+        for (let name of arr) {
+            if (name.length > max) {
+                max = name.length;
+            }
+        }
+        result += drawTopBorder(max+2) + "\n" 
+        for (let i=0; i < arr.length; i++) {
+            let namestr = arr[i] + " ".repeat(max-arr[i].length);
+            if (i === arr.length -1) {
+                result += drawBarsAround(namestr) + "\n" + drawBottomBorder(max+2);
+            } else {
+                result += drawBarsAround(namestr) + "\n" + drawMiddleBorder(max+2) + "\n"
+            }
+        }
+        return result;
+    }
+    
+}
 
-console.log(drawLine(0));
-console.log(drawTopBorder(2));
+process.argv.shift();
+process.argv.shift();
+console.log(boxIt(process.argv));
